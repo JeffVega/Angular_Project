37b23f4131129fdbf11dbfeac99fd14ca18b3af2
JeffVega <jeff.vega321@gmail.com>
2018-12-10 19:54:16 -0800

    connected posts data service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DataService} from '../data.service';
+import {Observable} from 'rxjs';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
-
-  constructor() { }
+  post$:Object
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getPost().subscribe(
+      data => this.post$ = data
+    )
   }
 
 }
